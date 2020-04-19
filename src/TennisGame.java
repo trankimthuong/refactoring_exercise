a97015437d7f611c8ad8c239a5ddcd245a485574
trankimthuong <thuongtk.bk@gmail.com>
2020-04-19 14:24:15 +0700

    refactoring exercise

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,44 +1,22 @@
 public class TennisGame {
 
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
+    public static String getScore(String player1Name, String player2Name, int player1Score, int player2Score) {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        if (player1Score==player2Score)
         {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
+            score = getScore(player1Score);
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (player1Score>=4 || player2Score>=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            score = getMinusScore(player1Score, player2Score);
         }
         else
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = player1Score;
+                else { score+="-"; tempScore = player2Score;}
                 switch(tempScore)
                 {
                     case 0:
@@ -55,6 +33,40 @@ public class TennisGame {
                         break;
                 }
             }
+        }
+        return score;
+    }
+
+    private static String getMinusScore(int player1Score, int player2Score) {
+        String score;
+        int minusResult = player1Score-player2Score;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
+    }
+
+    private static String getScore(int playerScore) {
+        String score;
+        switch (playerScore)
+        {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            case 3:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
         }
         return score;
     }
